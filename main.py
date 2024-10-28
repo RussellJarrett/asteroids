4a8b617ae6af029a4cabf5e90c219eda12f7ea24
russelljarrett <russell.j.jarrett@gmail.com>
2024-10-28 23:35:58 +1100

    Refactored

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from constants import *
 from player import Player
 
 def main():
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     player.draw(screen)
 
@@ -27,11 +29,13 @@ def main():
         # Clear screen each frame
         screen.fill((0, 0, 0))
             
-        # update player
-        player.update(dt)
+        for obj in updatable:
+            # update the objects
+            obj.update(dt)
         
-        # Draw player
-        player.draw(screen)
+        for obj in drawable:
+            # draw the objects
+            obj.draw(screen)
 
         #Flip the display
         pygame.display.flip()

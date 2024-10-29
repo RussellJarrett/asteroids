a18021e87137a11caedf24240f5bf544d1d4aca6
russelljarrett <russell.j.jarrett@gmail.com>
2024-10-29 12:39:16 +1100

    Limited fire rate

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.position = pygame.Vector2(x, y)
         self.rotation = 0
+        self.shoot_timer = 0.0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,9 +28,13 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity += pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    def shoot(self, dt):
+        self.shoot_timer += dt
+
+        if self.shoot_timer > PLAYER_SHOOT_COOLDOWN:
+            self.shoot_timer = 0.0
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity += pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -43,4 +48,4 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            self.shoot(dt)
